6ec9db44864cf2e1376e667330d0ba11b5975a12
Damar Wisnu <damarwisnu110906@gmail.com>
2026-04-01 19:11:57 +0700

    fixing

---
+++ app/Models/Portofolio.php
@@ -0,0 +1,5 @@
+<?php
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
---
+++ routes/api.php
@@ -0,0 +1,4 @@
+<?php
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
