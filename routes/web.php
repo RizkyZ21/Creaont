535865192d8f8de9483ce1d75c528d24ce45e436
Damar Wisnu <damarwisnu110906@gmail.com>
2026-04-01 18:34:30 +0700

    struktur file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/profile', function () {
+        return view('profile.index');
+    });
+
+    // Designer
+    Route::middleware(['role:designer'])->group(function () {
+        Route::get('/designer/dashboard', function () {
+            return view('designer.dashboard');
+        });
+
+        Route::resource('/portfolio', PortfolioController::class);
+    });
+
+    // Admin
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/admin/dashboard', [DashboardController::class, 'index']);
+        Route::resource('/admin/users', UserController::class);
+    });
 });
