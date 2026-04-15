3b9e7e9cfb724cd52ff83519c5b87cc1d686933b
Damar Wisnu <damarwisnu110906@gmail.com>
2026-04-15 22:31:32 +0700

    CRUD desain/porto

--- resources/views/designer/portofolio/create.blade.php
+++
@@ -1,8 +0,0 @@
-<form method="POST" enctype="multipart/form-data">
-    @csrf
-    <input name="title" placeholder="Judul">
-    <textarea name="description"></textarea>
-    <input type="file" name="image">
-    <input name="price" placeholder="Harga">
-    <button type="submit">Simpan</button>
-</form>
