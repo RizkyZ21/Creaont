c8bba00f72ffc9b75e1f6baa56a7737041d8e629
RizkyZ21 <rizkyzuhriansyah4@gmail.com>
2026-04-18 13:39:19 +0700

    Delete resources directory

--- resources/views/designer/portfolio/index.blade.php
+++
@@ -1,9 +0,0 @@
-<a href="/designer/portfolio/create">+ Tambah</a>
-
-@foreach($portfolios as $p)
-    <div>
-        <h3>{{ $p->title }}</h3>
-
-        <a href="/designer/portfolio/{{ $p->id }}/edit">Edit</a>
-    </div>
-@endforeach
